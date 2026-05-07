3ee264f30991214019e030a0b8bb57636b7a7a4c
pupu <nhinnhuphu@gmail.com>
2026-05-07 17:52:35 +0700

    fix: reduce threshold

--- src/train.py
+++ src/train.py
@@ -8,8 +8,7 @@ import os
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, f1_score
 
-EVAL_THRESHOLD = 0.70
-
+EVAL_THRESHOLD = 0.65
 
 def train(
     params: dict,
